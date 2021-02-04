a496c6f6a71f9cb01f102a0b3de964785ceb6106
Lhay Santiago <ryan.santiago@jeonsoft.com>
2021-02-05 00:39:06 +0800

    Updates

---
+++ precache-manifest.7b1c6b88d9bee2e687ca9ca6c783b2fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128907bd45e4bbb5c5ddfb2380e4dfe6",
+    "url": "/ryan-santiago/index.html"
+  },
+  {
+    "revision": "f6d76c7cc043d57c1bce",
+    "url": "/ryan-santiago/static/css/2.7a351efc.chunk.css"
+  },
+  {
+    "revision": "35febdc6c12ef6c6c293",
+    "url": "/ryan-santiago/static/css/main.90049a60.chunk.css"
+  },
+  {
+    "revision": "f6d76c7cc043d57c1bce",
+    "url": "/ryan-santiago/static/js/2.c45bba26.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/ryan-santiago/static/js/2.c45bba26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35febdc6c12ef6c6c293",
+    "url": "/ryan-santiago/static/js/main.2c04d25c.chunk.js"
+  },
+  {
+    "revision": "e1698ecd8062427737e5",
+    "url": "/ryan-santiago/static/js/runtime-main.ff90d294.js"
+  }
+]);
