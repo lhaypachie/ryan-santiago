43d585995da19f0b0a9d79d1eec678235271f1ef
Lhay Santiago <ryan.santiago@jeonsoft.com>
2021-02-05 23:24:34 +0800

    add breaking bad api

--- src/Components/PortfolioData.js
+++ src/Components/PortfolioData.js
@@ -5,9 +5,9 @@ export const PortfolioData = [
         alt: 'GettingOverIt'
     },
     {
-        image:  '/portfolio1.jpg',
-        url: 'https://lhaypachie.github.io/GettingOverIt/',
-        alt: 'GettingOverIt'
+        image:  '/breakingbad.jpg',
+        url: 'https://lhaypachie.github.io/ExampleAPI/',
+        alt: 'BreakingBad'
     },
     {
         image:  '/portfolio1.jpg',
